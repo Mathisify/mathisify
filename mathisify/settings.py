26699e017322a4b87e942e6e01be21156b37545e
MayuriDev <dylaniskandar23@gmail.com>
2020-07-25 15:41:48 -0700

    debug

--- mathisify/settings.py
+++ mathisify/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r7jggw!9&p^u%a&#51s-s)1^tc+m=da=lz%0jfcy@m$l4223on'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
